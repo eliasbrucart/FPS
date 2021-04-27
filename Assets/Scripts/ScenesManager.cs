bbcd56784276bf59c141250dc73876cb32c22c0c
Elias <eliasbrucart@hardgames.com.ar>
2021-04-27 12:44:40 -0300

    Added Menu scene

---
+++ Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    static public ScenesManager instanceScenesManager;
+
+    static public ScenesManager Instcane { get { return instanceScenesManager; } }
+
+    private void Awake()
+    {
+        if(instanceScenesManager != null && instanceScenesManager != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instanceScenesManager = this;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+}
