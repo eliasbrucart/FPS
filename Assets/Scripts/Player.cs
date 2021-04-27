b519235628e20f546fde1e790102df9908e0e26f
Elias <eliasbrucart@hardgames.com.ar>
2021-04-27 15:46:50 -0300

    Added player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int life;
+    public int Life 
+    {
+        get
+        {
+            return Life;
+        }
+        set
+        {
+            life = value;
+        }
+    }
+    void Start()
+    {
+        life = 100;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public bool IsAlive()
+    {
+        if (life <= 0)
+            return false;
+        return true;
+    }
+}
