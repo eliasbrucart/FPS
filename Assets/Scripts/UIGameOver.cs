fe13ddd315a2ee13dd4f605dcb6deee96bdea9ac
Elias <eliasbrucart@hardgames.com.ar>
2021-04-27 23:55:58 -0300

    Points earned are now printed in game over

---
+++ Assets/Scripts/UIGameOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGameOver : MonoBehaviour
+{
+    public Text pointsEarned;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointsEarned.text = "Points Earned: " + GameManager.instanceGameManager.Score;
+    }
+}
