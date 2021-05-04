d2da05bb0c3cd1a9b9b2a6272fc535790b6b7771
Elias <eliasbrucart@hardgames.com.ar>
2021-05-03 22:12:41 -0300

    Basic game UI

---
+++ Assets/Scripts/UIGamePlay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGamePlay : MonoBehaviour
+{
+    public Text pointsText;
+    public Text healthText;
+
+    public Player player;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        pointsText.text = "Points: " + GameManager.instanceGameManager.Score;
+        healthText.text = "Health: " + player.Life;
+    }
+}
